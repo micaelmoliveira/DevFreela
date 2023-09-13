633a077fa6c656e93f0f02bc32a598a1f1e9987b
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2023-09-13 11:06:42 -0300

    Input Model e View Model

--- DevFreela.Application/InputModels/UpdateProjectInputModel.cs
+++ DevFreela.Application/InputModels/UpdateProjectInputModel.cs
@@ -8,5 +8,9 @@ namespace DevFreela.Application.InputModels
 {
     public class UpdateProjectInputModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal TotalCost { get; set; }
     }
 }
