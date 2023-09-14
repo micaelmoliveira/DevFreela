768cf0e35b55c103ca0d82e8f6fdb48d504ee22b
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2023-09-14 09:36:48 -0300

    Teste com dapper

--- DevFreela.Application/Services/Implementations/SkillService.cs
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -12,7 +12,7 @@ namespace DevFreela.Application.Services.Implementations
         private readonly DevFreelaDbContext _dbContext;
         private readonly string _connectionString;
 
-        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration )
+        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
             _connectionString = configuration.GetConnectionString("DevFreelaCs")!;
