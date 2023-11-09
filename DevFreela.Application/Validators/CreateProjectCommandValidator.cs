430a2aadf8f191cf378059327c728c2735283491
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2023-11-09 17:29:16 -0300

    Criação dos validators de create project e create user

---
+++ DevFreela.Application/Validators/CreateProjectCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
+    {
+        public CreateProjectCommandValidator()
+        {
+            RuleFor(p => p.Description)
+                .MaximumLength(255)
+                .WithMessage("Tamanha máximo de Descrição é de 255 caracteres.");
+
+            RuleFor(p => p.Title)
+                .MaximumLength(30)
+                .WithMessage("Tamanha máximo de Títuto é de 30 caracteres.");
+        }
+    }
+}
---
+++ DevFreela.Application/Validators/CreateUserCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.Commands.CreateUser;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Validators
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(p => p.Email)
+                .EmailAddress()
+                .WithMessage("E-mail em formato inválido");
+
+            RuleFor(p => p.Password)
+                .Must(ValidPassword)
+                .WithMessage("A senha deve conter um número, uma letra maiúscula, uma minúscula, um caracter especial e ser mais que 8 caracteres");
+
+            RuleFor(p => p.FullName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("O nome é obrigatório");
+        }
+
+        private bool ValidPassword(string password)
+        {
+            var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+            return regex.IsMatch(password);
+        }
+    }
+}
