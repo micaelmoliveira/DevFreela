21fa6dd7712978029bacca77539c743158350cfd
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2023-11-09 17:26:19 -0300

    Adicionado o atributo ApiController

--- DevFreela.API/Controllers/SkillsController.cs
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DevFreela.API.Controllers
 {
     [Route("api/skills")]
+    [ApiController]
     public class SkillsController : ControllerBase
     {
         private readonly IMediator _mediator;
