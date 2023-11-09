5ce577657ecbc3f4dadcb98e1b4c27a74c20a784
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2023-11-09 17:38:36 -0300

    Criação de IProjectRepository e ProjectRepository

--- DevFreela.Core/Repositories/IProjectRepository.cs
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -1,9 +1,4 @@
 ﻿using DevFreela.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevFreela.Core.Repositories
 {
@@ -11,5 +6,9 @@ namespace DevFreela.Core.Repositories
     {
         Task<List<Project>> GetAllAsync();
         Task<Project?> GetByIdAsync(int id);
+        Task AddAsync(Project project);
+        Task AddCommentAsync(ProjectComment projectComment);
+        Task StartAsync(Project project);
+        Task SaveChangesAsync();
     }
 }
--- DevFreela.Infra/Persistence/Repositories/ProjectRepository.cs
+++ DevFreela.Infra/Persistence/Repositories/ProjectRepository.cs
@@ -1,17 +1,22 @@
 ﻿using Azure.Core;
+using Dapper;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Infra.Persistence.Repositories
 {
     public class ProjectRepository : IProjectRepository
     {
         private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
 
-        public ProjectRepository(DevFreelaDbContext dbContext)
+        public ProjectRepository(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs")!;
 
         }
 
@@ -27,5 +32,35 @@ namespace DevFreela.Infra.Persistence.Repositories
                 .Include(p => p.Freelancer)
                 .SingleOrDefaultAsync(p => p.Id == id);
         }
+
+        public async Task AddAsync(Project project)
+        {
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddCommentAsync(ProjectComment projectComment)
+        {
+            await _dbContext.ProjectComments.AddAsync(projectComment);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task StartAsync(Project project)
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "UPDATE projects SET Status = @status, StartedAt = @startedat WHERE Id = @id";
+
+                await sqlConnection.ExecuteAsync(script, new { status = project.Status, startedat = project.StartedAt, project.Id });
+            }
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
     }
 }
