80b1e870b71ce2a4106e8e34a2e6b7aedf8aacdd
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-10 01:04:25 -0300

    Configurações dbcontext

---
+++ src/DevFreela.Infrastructure/Persistence/Configurations/ProjectCommentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class ProjectCommentConfiguration : IEntityTypeConfiguration<ProjectComment>
+    {
+        public void Configure(EntityTypeBuilder<ProjectComment> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+
+            builder
+                .HasOne(p => p.Project)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(p => p.ProjectId);
+
+            builder
+                .HasOne(p => p.User)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(p => p.UserId);
+        }
+    }
+}
--- src/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ src/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,7 +1,33 @@
-﻿namespace DevFreela.Infrastructure.Persistence
-{
-    public class DevFreelaDbContext
-    {
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext : DbContext
+    {
+        protected readonly IConfiguration _configuration; 
+
+        public DevFreelaDbContext(IConfiguration configuration, DbContextOptions<DevFreelaDbContext> options) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(_configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
