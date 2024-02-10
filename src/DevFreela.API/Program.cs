4f94a363336b11cfe49d60cee9384918b613d1ba
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-10 01:59:06 -0300

    Adicionando sqlite no program

--- src/DevFreela.API/Program.cs
+++ src/DevFreela.API/Program.cs
@@ -1,5 +1,7 @@
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DevFreelaDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
 
 builder.Services.AddScoped<IProjectService, ProjectService>();
 
