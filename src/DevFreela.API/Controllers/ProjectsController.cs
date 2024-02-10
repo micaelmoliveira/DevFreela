2514ae40a417df174a8a14bb635e12318bef6845
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-09 22:33:40 -0300

    Camada de api

---
+++ src/DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateProjectModel createProject)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ src/DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+        
+        [HttpPost]
+        public IActionResult Post(CreateUserModel createUser)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUser);
+        }
+
+        [HttpPut("{id}/login")]
+        public IActionResult Login(int id, LoginModel login)
+        {
+            return NoContent();
+        }
+    }
+}
