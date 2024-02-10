62a77b3fb2be194f398ea052e48e14031fddd622
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-09 23:15:08 -0300

    Alterações nos parâmetros de ProjectController

--- src/DevFreela.API/Controllers/ProjectsController.cs
+++ src/DevFreela.API/Controllers/ProjectsController.cs
@@ -1,5 +1,8 @@
 ﻿using DevFreela.API.Models;
+using DevFreela.Application.InputDTO;
+using DevFreela.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using static DevFreela.Application.InputDTO.Inputs;
 
 namespace DevFreela.API.Controllers
 {
@@ -7,31 +10,52 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
         [HttpGet]
         public IActionResult Get(string query)
         {
-            return Ok();
+            var projects = _projectService.GetAll(query);
+
+            return Ok(projects);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            return Ok();
+            var project = _projectService.GetById(id);
+
+            if(project is null) return NotFound();
+            
+            return Ok(project);
         }
 
         [HttpPost]
-        public IActionResult Post(CreateProjectModel createProject)
+        public IActionResult Post(NewProjectInputRecord input)
         {
-            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, UpdateProjectModel updateProject)
-        {
-            if(updateProject.Description.Length > 200)
+            if(input.Title.Length > 50)
             {
                 return BadRequest();
             }
+            var id = _projectService.Create(input);
+
+            return CreatedAtAction(nameof(GetById), new { id }, input);
+        }
+
+        [HttpPut]
+        public IActionResult Put(UpdateProjectInputRecord input)
+        {
+            if(input.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            _projectService.Update(input);
 
             return NoContent();
         }
@@ -39,11 +63,13 @@ namespace DevFreela.API.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            _projectService.Delete(id);
+
             return NoContent();
         }
 
-        [HttpPost("{id}/comments")]
-        public IActionResult PostComment(int id, CreateCommentModel createComment)
+        [HttpPost("comments")]
+        public IActionResult PostComment(CreateCommentInputRecord input)
         {
             return NoContent();
         }
@@ -51,12 +77,16 @@ namespace DevFreela.API.Controllers
         [HttpPut("{id}/start")]
         public IActionResult Start(int id)
         {
+            _projectService.Start(id);
+
             return NoContent();
         }
 
         [HttpPut("{id}/finish")]
         public IActionResult Finish(int id)
         {
+            _projectService.Finish(id);
+
             return NoContent();
         }
     }
