3cd354d4322e526dce9234633080012fedfa589c
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-09 22:34:22 -0300

    Camada core

---
+++ src/DevFreela.Core/Entities/Project.cs
@@ -0,0 +1,58 @@
+﻿using DevFreela.Core.Enums;
+
+namespace DevFreela.Core.Entities
+{
+    public class Project : BaseEntity
+    {
+        public Project(string title, string description, int clientId, int freelancerId, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            ClientId = clientId;
+            FreelancerId = freelancerId;
+            TotalCost = totalCost;
+
+            CreatedAt = DateTime.Now;
+            Status = ProjectStatusEnum.Created;
+            Comments = [];
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int ClientId { get; private set; }
+        public int FreelancerId { get; private set; }
+        public decimal TotalCost { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime? FinishedAt { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
+
+        public void Start()
+        {
+            if (Status == ProjectStatusEnum.Created)
+            {
+                Status = ProjectStatusEnum.InProgress;
+                StartedAt = DateTime.Now;
+            }
+        }
+
+        public void Finish()
+        {
+            if (Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Finished;
+                FinishedAt = DateTime.Now;
+            }
+        }
+
+        public void Cancel()
+        {
+            if(Status == ProjectStatusEnum.Created || Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Cancelled;
+            }
+        }
+
+    }
+}
