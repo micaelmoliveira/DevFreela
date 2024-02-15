1ad13225c500f8f33f817b227f2762e6a2f4079d
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-15 00:39:36 -0300

    Adicionado Repository

---
+++ src/DevFreela.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -0,0 +1,46 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+
+        public ProjectRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Project>> GetAllAsync()
+        {
+            return await _dbContext.Projects.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Project?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Projects
+                        .Include(p => p.Client)
+                        .Include(p => p.Freelancer)
+                        .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task AddAsync(Project project)
+        {
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddCommentAsync(ProjectComment projectComment)
+        {
+            await _dbContext.ProjectComments.AddAsync(projectComment);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
