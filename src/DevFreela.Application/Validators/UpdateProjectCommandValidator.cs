e1a8eeb9ad32667f8506739ae29396205b13be16
Micael Moura de Oliveira <micaelmoura.oliveira@gmail.com>
2024-02-15 02:08:07 -0300

    Adicionado os validators

---
+++ src/DevFreela.Application/Validators/UpdateProjectCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.Application.Commands.ProjectCommand.UpdateProject;
+using FluentValidation;
+
+namespace DevFreela.Application.Validators
+{
+    public class UpdateProjectCommandValidator : AbstractValidator<UpdateProjectCommand>
+    {
+        public UpdateProjectCommandValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage("Título é obrigatório")
+                .MaximumLength(30)
+                .WithMessage("Tamanho máximo de Título é de 30 caracteres");
+                
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .WithMessage("Descrição é obrigatória")
+                .MaximumLength(255)
+                .WithMessage("Tamanho máximo de Título é de 30 caracteres");
+
+            RuleFor(p => p.TotalCost)
+                .GreaterThan(0)
+                .WithMessage("Custo total deve ser maior que zero");
+        }
+    }
+}
